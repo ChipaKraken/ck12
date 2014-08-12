2589348aa9bc11abd8c493795a7b21a3f8ef5135
Chyngyz Dyikanbaev <chipalino@live.com>
2014-08-12 15:33:21 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+if (!Object.prototype.watch) {
+  Object.defineProperty(Object.prototype, "watch", {
+      enumerable: false
+    , configurable: true
+    , writable: false
+    , value: function (prop, handler) {
+      var
+        oldval = this[prop]
+      , newval = oldval
+      , getter = function () {
+        return newval;
+      }
+      , setter = function (val) {
+        oldval = newval;
+        return newval = handler.call(this, prop, oldval, val);
+      }
+      ;
+      
+      if (delete this[prop]) { // can't watch constants
+        Object.defineProperty(this, prop, {
+            get: getter
+          , set: setter
+          , enumerable: true
+          , configurable: true
+        });
+      }
+    }
+  });
+}
+ 
+// object.unwatch
+if (!Object.prototype.unwatch) {
+  Object.defineProperty(Object.prototype, "unwatch", {
+      enumerable: false
+    , configurable: true
+    , writable: false
+    , value: function (prop) {
+      var val = this[prop];
+      delete this[prop]; // remove accessors
+      this[prop] = val;
+    }
+  });
+}
+
 angular.module('app', ['ui.router']).config([
   '$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
     $urlRouterProvider.otherwise('/');
@@ -28,16 +72,21 @@ angular.module('app', ['ui.router']).config([
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     }
-    if ($location.path() === '/bar'){
-      document.getElementsByTagName('body')[0].className = "bar_bg";
-    }
-    else if($location.path() === '/cave'){
-      document.getElementsByTagName('body')[0].className = "cave_bg";
-    }
-    else if($location.path() === '/rest'){
-      document.getElementsByTagName('body')[0].className = "rest_bg";
-    }
-    else{
-      document.getElementsByTagName('body')[0].className = "def_bg";
-    };
+    $scope.path = $location.path(); 
+    // if ($location.path() === '/bar'){
+    //   document.getElementsByTagName('body')[0].className = "bar_bg";
+    // }
+    // else if($location.path() === '  /cave'){
+    //   document.getElementsByTagName('body')[0].className = "cave_bg";
+    // }
+    // else if($location.path() === '/rest'){
+    //   document.getElementsByTagName('body')[0].className = "rest_bg";
+    // }
+    // else{
+    //   document.getElementsByTagName('body')[0].className = "def_bg";
+    // };
+    $scope.watch('path', function (id, oldval, newval) {
+      console.log( "o." + id + " changed from " + oldval + " to " + newval );
+      return newval;
+    })
 }]);
